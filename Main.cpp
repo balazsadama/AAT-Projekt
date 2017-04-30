4900fdb0897c33975c18873a53b1537ccc345d5c
balazsadama <balazs.adam.a@gmail.com>
2017-04-30 20:32:47 +0300

    Started working on Main (azon belul menun)

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include "KorkorosLista.h"
+
+void menuFelsorolas() {
+	cout << "1. Letrehoz\n";
+	cout << "2. Feltolt\n";
+	cout << "3. Lekerdez egy modul tartalmat\n";
+	cout << "4. Ures-e\n";			// ez a ketto 
+	cout << "5. Tele van-e\n";		// lehet nem kell ide
+	cout << "6. Uj modul beszurasa\n";
+	cout << "7. Modul torlese\n";
+	cout << "0. Kilepes\n";
+}
+
+int main() {
+
+}
